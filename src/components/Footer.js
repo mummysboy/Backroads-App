61b48737cc31c0a0b03003291577065035bc08d5
mummysboy <isaachirsch@gmail.com>
2023-01-06 04:19:06 +0200

    First commit

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import { socialLinks } from "../data";
+import PageLinks from "./PageLinks";
+
+const Footer = () => {
+  return (
+    <footer className="section footer">
+      <PageLinks parentClass="footer-links" itemClass="footer-link" />
+
+      <ul className="footer-icons">
+        {socialLinks.map((link) => {
+          const { id, href, icon } = link;
+          return (
+            <socialLinks key={link.id} {...link} itemClass="footer-icon" />
+          );
+        })}
+      </ul>
+      <p className="copyright">
+        copyright &copy; Backroads travel tours company
+        <span id="date">{new Date().getFullYear()}</span> all rights reserved
+      </p>
+    </footer>
+  );
+};
+export default Footer;
